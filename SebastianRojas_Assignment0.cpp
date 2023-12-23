364df6040e874f7a833df5c3ec1ee74549f3d590
SebasRojassSacai <154623036+SebasRojassSacai@users.noreply.github.com>
2023-12-23 13:42:20 -0500

    Add files via upload

---
+++ SebastianRojas_Assignment0.cpp
@@ -0,0 +1,17 @@
+/*
+Assignment 0: Goal is to learn how to submit assignments onto Canvas.
+Name: Sebastian Rojas
+Date: 08/28/2023
+*/
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string A;
+    std::cout << "Hello Class whats your name?\n";
+    std::cout << "Please put your name here: \n";
+    std::cin >> A;
+    std::cout << "So your name is: " << A << "\n";
+    std::cout << "What a good name! \n";
+}
